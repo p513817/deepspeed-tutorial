33151a322ed23a414cf21f1b7a139e62c8bd89c1
MaxChang <max_chang@innodisk.com>
2024-07-23 10:14:24 +0000

    feat: add llama3-80b peft verication

---
+++ training/llama3-70b/peft-with-llama3.py
@@ -0,0 +1,26 @@
+import torch
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForCausalLM
+
+peft_config = LoraConfig(
+    task_type=TaskType.SEQ_2_SEQ_LM,
+    inference_mode=False,
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.1,
+)
+peft_config = LoraConfig(
+    task_type=TaskType.SEQ_2_SEQ_LM,
+    inference_mode=False,
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.1,
+)
+model = AutoModelForCausalLM.from_pretrained(
+    "meta-llama/Meta-Llama-3-70B",
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+)
+
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
